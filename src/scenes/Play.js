1a36e6fdd97d5e1a69729afaebaf82a434a9244c
Filip Jerga <jerga99@gmail.com>
2020-10-20 21:49:09 +0200

    Adds player

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -7,12 +7,13 @@ class PlayScene extends Phaser.Scene {
   }
 
   preload () {
-    debugger
     this.load.image('sky', 'assets/sky.png');
+    this.load.image('player', 'assets/player.png')
   }
 
   create () {
     this.add.image(400, 300, 'sky');
+    this.physics.add.sprite(400, 300, 'player');
   }
 }
 
