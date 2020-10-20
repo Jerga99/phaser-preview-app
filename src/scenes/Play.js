8ed7f08c951f292ef524b0fe95f2832d3ddba658
Filip Jerga <jerga99@gmail.com>
2020-10-20 22:12:35 +0200

    creates simple movements

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -13,7 +13,31 @@ class PlayScene extends Phaser.Scene {
 
   create () {
     this.add.image(400, 300, 'sky');
-    this.physics.add.sprite(400, 300, 'player');
+    this.speed = 500;
+    this.player = this.physics.add.sprite(400, 300, 'player');
+    this.player.setCollideWorldBounds(true);
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+    const { up, down, left, right } = this.cursors;
+
+    if (up.isDown) {
+      this.player.setVelocityY(-this.speed);
+      this.player.setVelocityX(0);
+    } else if (down.isDown) {
+      this.player.setVelocityY(this.speed);
+      this.player.setVelocityX(0);
+    } else if (left.isDown) {
+      this.player.setVelocityX(-this.speed);
+      this.player.setVelocityY(0);
+    } else if (right.isDown) {
+      this.player.setVelocityX(this.speed);
+      this.player.setVelocityY(0);
+    } else {
+      this.player.setVelocity(0);
+    }
   }
 }
 
